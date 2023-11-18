2bdf6b9c40af022fd02ca945acac99f08d18a0f5
Sultan Dzjumajev <Sultan.Dzjumajev@norgesgruppen.no>
2023-11-19 00:43:27 +0100

    DomainEvent inherits INotification

--- src/src/library.domain/Base/DomainEvent.cs
+++ src/src/library.domain/Base/DomainEvent.cs
@@ -1,2 +1,5 @@
-﻿namespace library.domain.Base;
-public abstract record DomainEvent(Guid Id);
+﻿using MediatR;
+
+namespace library.domain.Base;
+
+public abstract record DomainEvent(Guid Id) : INotification;
